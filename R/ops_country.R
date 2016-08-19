ab481ee24854af8faa70a14079c33d4da7a208cf
Paul Oldham <poldham@mac.com>
2016-08-19 09:51:55 +0100

    specifying import from dplyr filter. Note that original was non-specific on filter and could have been using stats filter causing an issue.

--- R/ops_country.R
+++ R/ops_country.R
@@ -13,10 +13,12 @@
 #'   regional patent offices from WIPO standard St.3
 #'   \url{http://www.wipo.int/pct/guide/en/gdvol1/annexes/annexk/ax_k.pdf}.
 #'   Continent and region fields are assigned to patent at present.
+#' @importFrom dplyr select_
+#' @importFrom dplyr filter
 #' @examples \dontrun{ops_country(data = pizza_numbers, col = "country")}
 ops_country <- function(data, col){
-  src <- dplyr::select_(data, col)
-  tmp <- dplyr::select_(countrycode_data, "country_name", "iso2c")
+  src <- select_(data, col)
+  tmp <- select_(countrycode_data, "country_name", "iso2c")
   tmp$status <- countrycode_data$iso2c %in% src$country
   res <- filter(tmp, status == TRUE) #iso2c for by-y
   res$status <- NULL
@@ -24,6 +26,8 @@ ops_country <- function(data, col){
   #print(data) intrusive and dropped.
   }
 #note, the specification of country in the last line could vary between datasets e.g as publication_country, priority_country, application_country. So a better solution is needed here as this only works where there is a column called country.
+
+#problem that the code is producing a lot of extra columns with . in them as a join. So the code needs more work. It may be that not specifying dplyr for filter was causing the problem.
 
 #original working code for reference
 #tmp <- dplyr::select_(countrycode_data, "country_name", "iso2c")
